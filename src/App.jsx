feda0ac1cfd16ba27968202c6282d966a64b950e
Harsh Bhardwaj <harshbhardwaj9087@gmail.com>
2024-11-02 21:58:13 +0530

    uploaded files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Posts } from "./Components/Posts";
+
+const App = () => {
+  return (
+    <section className="main-section">
+      <Posts />
+    </section>
+  );
+};
+
+export default App;
